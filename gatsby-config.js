c2d755379f8568f5e52bbadae51286ad755c2184
Brandon Nguyen <gnuyent@protonmail.com>
2021-08-06 17:15:17 -0700

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "SDSU CDT Website",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'SDSU CDT Website',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
---
+++ src/components/sections/alumni.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BaseSection from './base';
+
+const Alumni = () => <BaseSection title="Alumni" />;
+
+export default Alumni;
